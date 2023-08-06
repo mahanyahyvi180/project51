e82fa25115b11b18ce0a89d020bd2ca620652644
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-06 22:51:15 +0330

    Add files via upload

---
+++ ConsoleApp51/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Giraff Aacademy";
+
+            char grade = 'A';
+
+            
+            int Age = 30;
+            double gpa = 3.3;
+            bool isMale = true;
+            Console.WriteLine(30);
+            Console.ReadLine();
+        }
+    }
+}
